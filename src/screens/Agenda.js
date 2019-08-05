ee649b7741ca52c99970a63cd792acc13e48a16b
micaelbarreto <micaelplayer98@gmail.com>
2019-08-05 00:46:52 +0000

    frony with asyncStorage

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -1,24 +1,108 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import moment from 'moment';
-import todayImage from '../../assets/imgs/today.jpg';
-import commonStyles from '../commonStyles';
+import React, { Component } from 'react'
+import { StyleSheet, 
+         Text, 
+         View, 
+         ImageBackground, 
+         FlatList,
+         TouchableOpacity,
+         Platform,
+         AsyncStorage
+} from 'react-native'
+import moment from 'moment'
+import todayImage from '../../assets/imgs/today.jpg'
+import commonStyles from '../commonStyles'
+import Task from '../components/Task'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import ActionButton from 'react-native-action-button'
+import AddTask from './AddTask'
 
 export default class Agenda extends Component{
+    state = {
+        tasks: [
+            
+        ],
+        visibleTasks: [],
+        showDoneTask: true,
+        showAddTask: false,
+    }
+
+    addTask = task => {
+        const tasks = [...this.state.tasks]
+        tasks.push({
+            id: Math.random(),
+            desc: task.desc,
+            estimatedAt: task.date,
+            donaAt: null
+        })
+        this.setState({ tasks, showAddTask: false }, this.filterTasks)
+    }
+
+    deleteTask = id => {
+        const tasks = this.state.tasks.filter(task => task.id !== id)
+        this.setState({ tasks }, this.filterTasks)
+    }
+
+    filterTasks = () => {
+        let visibleTasks = null
+        if(this.state.showDoneTask) {
+            visibleTasks = [...this.state.tasks]
+        } else {
+            const pending = task => task.doneAt == null
+            visibleTasks = this.state.tasks.filter(pending)
+        }
+        this.setState({ visibleTasks })
+        AsyncStorage.setItem('tasks', JSON.stringify(this.state.tasks))
+    }
+
+    toggleFilter = () => {
+        this.setState({ showDoneTask: !this.setState.showDoneTask }, this.filterTasks)
+    }
+
+    componentDidMount = async () => {
+        const data = await AsyncStorage.getItem('tasks')
+        const tasks = JSON.parse(data) || []
+        this.setState({ tasks }, this.filterTasks)
+    }
+
+    toggleTask = id => {
+        this.state.tasks.map(task => {
+            if(task.id == id) {
+                task = {...task}
+                task.doneAt = task.doneAt ? null : new Date()
+            }
+            return task
+        })
+
+        this.setState({ tasks }, this.filterTasks)
+    }
+
     render() {
         return (
             <View style={styles.container}>
+                <AddTask isVisible={this.state.showAddTask}
+                    onSave={this.addTask}
+                    onCancel={() => this.setState({ showAddTask: false })} />
                 <ImageBackground source={todayImage} style={styles.background}>
+                    <View style={styles.iconBar}>
+                        <TouchableOpacity onPress={() => this.toggleFilter}>
+                            <Icon name={this.state.showDoneTask ? 'eye' : 'eye-slash'} 
+                                size={20}
+                                color={commonStyles.colors.secondary} />
+                        </TouchableOpacity>
+                    </View>
                     <View style={styles.titleBar}>
                         <Text style={styles.title}>Today</Text>
-                        <Text style={styles.title}>{moment().format('ddd, D [of] MMMM')}</Text>
+                        <Text style={styles.title}>{moment().format('ddd, D [of] MMMM [of] YYYY')}</Text>
                     </View>
                 </ImageBackground>
                 <View style={styles.taskContainer}>
-                    <Text>Task 1</Text>
-                    <Text>Task 2</Text>
-                    <Text>Task 3</Text>
+                    <FlatList data={this.state.visibleTasks}
+                        keyExtractor={item => `${item.id}`}
+                        renderItem={({ item }) => <Task {...item} toggleTask={this.toggleTask} onDelete={this.deleteTask} />}
+                    />
                 </View>
+                <ActionButton buttonColor={commonStyles.colors.today}
+                    onPress={() => { this.setState({ showAddTask: true }) }} />
             </View>
         );
     }
@@ -51,5 +135,11 @@ const styles = StyleSheet.create({
     },
     taskContainer: {
         flex: 7
+    },
+    iconBar: {
+        marginTop: Platform.OS === 'ios' ? 30 : 10,
+        marginHorizontal: 20,
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
     }
 });
